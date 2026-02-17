1280ead12bf5c2e2d5d2f6e0d2d4a4d078e75815
Bohdan Trach <bohdan.trach@huawei.com>
2026-02-17 17:32:54 +0100

    Allow configuring host and port at runtime.

--- bm-generator/05_generate.sh
+++ bm-generator/05_generate.sh
@@ -2,7 +2,5 @@
 # Copyright (C) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-
-cmake -S. -Bbuild
 cmake --build build --target csb-generate
 cmake --build build --target csb-gen-agg
